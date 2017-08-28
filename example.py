4894cc1561c74eae29ea84196c369773165574da
Anthony Shaw <anthonyshaw@apache.org>
2017-08-28 14:36:07 +1000

    update example code

--- example.py
+++ example.py
@@ -1,9 +1,24 @@
 from pathgather import PathgatherClient
 import yaml
+import json
+
 
 with open('.tenant.yml', 'r') as tenant_yml:
     config = yaml.load(tenant_yml)
 
 client = PathgatherClient(config['host'], config['api_key'])
 
-print(client.users.get_all_users())
+print(client.users.all())
+
+
+with open('dump.json', 'r') as dump_j:
+    data = json.load(dump_j)
+
+for user in data['users']:
+    print('Creating {0}'.format(user['full_name']))
+    new_user = client.users.create(
+        name=user['full_name'], 
+        job_title=user['job_title'],
+        department='Learning and Development',
+        email=user['email'])
+    print(new_user)
