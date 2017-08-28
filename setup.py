0e5711d509d31e8f243a56c38a190f0d6a66edda
Anthony Shaw <anthonyshaw@apache.org>
2017-08-28 14:36:12 +1000

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='pathgather',
-    version='0.1.0',
+    version='0.2.0',
     description="API client for PathGather",
     long_description=readme + '\n\n' + history,
     author="Anthony Shaw",
