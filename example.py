22ec14df057c883993dbdc92b021d50adee8cc5f
Anthony Shaw <anthonyshaw@apache.org>
2017-09-01 13:39:55 +1000

    update example

--- example.py
+++ example.py
@@ -6,19 +6,6 @@ from pprint import pprint
 with open('.tenant.yml', 'r') as tenant_yml:
     config = yaml.load(tenant_yml)
 
-client = PathgatherClient(config['host'], config['api_key'], 'https://localhost:8888/', True)
+client = PathgatherClient(config['host'], config['api_key'])
 
 pprint(client.users.all())
-
-
-#with open('dump.json', 'r') as dump_j:
-#    data = json.load(dump_j)
-
-#for user in data['users']:
-#    print('Creating {0}'.format(user['full_name']))
-#    new_user = client.users.create(
-#        name=user['full_name'], 
-#        job_title=user['job_title'],
-#        department='Learning and Development',
-#        email=user['email'])
-#    print(new_user)
